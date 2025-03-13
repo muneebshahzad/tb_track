41bea97b6869fe1306030452a7ca2360c735bb43
muneebshahzad <77743328+muneebshahzad@users.noreply.github.com>
2025-03-13 18:46:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ LAST_REQUEST_TIME = 0
 
 async def getShopifyOrders():
     global order_details
-    orders = shopify.Order.find(limit=5, order='created_at ASC')
+    orders = shopify.Order.find(limit=250, order='created_at ASC')
     order_details = []
     total_start_time = time.time()
 

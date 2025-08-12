bf1bdf0d6996030850bf73d2b6245ee8992f1a31
muneebshahzad <77743328+muneebshahzad@users.noreply.github.com>
2025-08-12 19:18:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -702,7 +702,7 @@ def pending_orders_mobile():
     for shopify_order in order_details:
         if any(tag.startswith("Dispatched") for tag in shopify_order.get('tags', [])):
             continue
-        if shopify_order['status'] in ['Booked', 'Un-Booked']:
+        if shopify_order.get('status') in ['Booked', 'Un-Booked']:
             shopify_items_list = []
             for item in shopify_order['line_items']:
                 track_num = item.get('tracking_number', 'N/A')
@@ -903,4 +903,5 @@ if __name__ == "__main__":
 
     # Start Flask app
     app.run(host="0.0.0.0", port=5001, debug=True)
+
 
